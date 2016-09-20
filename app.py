5b6b2b249882f1c9e35ad793232a967f383dfc92
qminh93 <qminh93@gmail.com>
2016-09-21 00:32:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
 import time
+import logging
 import telepot
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
+from pogo import util
+from pogo.api import PokeAuthSession
+from pogo.advanced_trainer import Champion
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
+    if content_type == 'text':
+        command = msg['text']
+        if command.startswith('/'):
+            if command == '/start':
+                bot.sendMessage(chat_id, 'Hello. I am your Pokemon Go Manager')
+            elif command == '/end':
+                bot.sendMessage(chat_id, 'Bye')
+            elif command.startswith('/manage'):
+                tokens = command.split()
+                if len(tokens) < 3:
+                    bot.sendMessage(chat_id, 'Not enough arguments')
+                else:
+                    usr = tokens[1]
+                    pwd = tokens[2]
+                    auth_session = PokeAuthSession(usr, pwd, 'google', './encrypt.so', geo_key = None)
+                    session = auth_session.authenticate()
 
-    bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
+                    if session:
+                        trainer = Champion(auth_session, session)
+                        trainer.getProfile()
+                        trainer.checkInventory()
+                        time.sleep(1)
+                        if tokens[0] == '/manage_items':
+                            trainer.manage_items(100, 50, 50, 200)
+                            bot.sendMessage(chat_id, 'Done.')
+                        elif tokens[0] == '/manage_pokemons':
+                            released_log = trainer.manage_pokemon()
+                            bot.sendMessage(chat_id,'Done.')
+                            for pokemon in released_log: bot.sendMessage(chat_id, pokemon)
+                        elif tokens[0] == '/manage_iv':
+                            trainer.measure_pokemon()
+                            bot.sendMessage(chat_id,'Done')
+                        else:
+                            bot.sendMessage(chat_id,'Invalid command.')
+            else:
+                bot.sendMessage(chat_id,'Invalid command.')
+
 
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
@@ -22,6 +58,9 @@ TOKEN = '283747572:AAEdfrOnYTglaSYAqx4g0kBPHlBV7IoEifw'  # get token from comman
 bot = telepot.Bot(TOKEN)
 bot.message_loop({'chat': on_chat_message,
                   'callback_query': on_callback_query})
+
+util.setupLogger()
+logging.debug('Logger set up')
 print('Listening ...')
 
 while 1:
